a988512580012329fb6f2a9c40ba30bd6f924930
Edward Bebbington <edward.bebbington@intercity.technology>
2020-07-21 15:33:55 +0100

    [fix-and-improve-tests] Added tests for client class

---
+++ tests/unit/client_test.ts
@@ -0,0 +1,26 @@
+import { Rhum } from "../deps.ts";
+import { Client } from "../../src/client.ts";
+import { WebSocket } from "../../deps.ts";
+
+const ClientSocket = () => {
+  return {
+    send: () => true,
+    close: () => true,
+  };
+};
+
+Rhum.testPlan("unit/client_test.ts",  () => {
+  Rhum.testSuite("constructor()", () => {
+    Rhum.testCase("Sets the name and creates empty listeners", () => {
+      const client = new Client(1, ClientSocket() as unknown as WebSocket)
+      Rhum.asserts.assertEquals(client.heartbeat_id, null)
+      Rhum.asserts.assertEquals(client.id, 1)
+      Rhum.asserts.assertEquals(client.listening_to, [])
+      Rhum.asserts.assertEquals(client.pong_received, false)
+      Rhum.asserts.assertEquals(typeof client.socket.send, "function")
+      Rhum.asserts.assertEquals(typeof client.socket.close, "function")
+    })
+  })
+})
+
+Rhum.run()
--- tests/unit/tests.ts
+++ tests/unit/tests.ts
@@ -1,4 +1,5 @@
 import "./event_emitter_test.ts";
 import "./server_test.ts";
 import "./reserved_event_names_test.ts"
-import "./channel_test.ts"
+import "./channel_test.ts";
+import "./client_test.ts"
