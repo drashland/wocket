9afc9928862cfc5605ba7aa0554c39a6136c7716
Eric Crooks <eric.crooks@gmail.com>
2020-07-26 14:09:23 -0400

    organize test deps

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,19 +1,11 @@
 export {
-  Drash,
-} from "https://deno.land/x/drash@v1.1.0/mod.ts";
-
-export {
   WebSocket,
-  WebSocketMessage,
   acceptWebSocket,
   connectWebSocket,
   isWebSocketCloseEvent,
-} from "https://deno.land/std@v0.61.0/ws/mod.ts";
+  serve,
+} from "../deps.ts";
 
 export {
   Rhum,
 } from "https://deno.land/x/rhum@v1.0.1/mod.ts";
-
-export {
-  serve,
-} from "https://deno.land/std@0.62.0/http/server.ts";
