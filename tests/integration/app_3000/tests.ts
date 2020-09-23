22669c910584d0cd2908cc0cca359b76a4d93be6
Edward Bebbington <edward.bebbington@intercity.technology>
2020-09-23 23:54:34 +0100

    Expand upon integration tests

--- tests/integration/app_3000/tests.ts
+++ tests/integration/app_3000/tests.ts
@@ -42,37 +42,30 @@ Rhum.testPlan("app_3000", () => {
   Rhum.testSuite("server", () => {
     Rhum.testCase("should allow clients to connect", async () => {
       const promise = deferred();
-
       const WSClient = new WebSocket(
         `ws://${WSServer.hostname}:${WSServer.port}`,
       );
-
       WSClient.onopen = function () {
         WSClient.send(JSON.stringify({
           connect_to: ["chan1"],
         }));
       };
-
       WSClient.onmessage = function (message: any) {
         Rhum.asserts.assertEquals(message.data, "Connected to chan1.");
         WSClient.close();
       };
-
       WSClient.onclose = function () {
         promise.resolve();
       };
-
       await promise;
     });
     Rhum.testCase(
-      "should allow messages to be sent back and forth",
+      "should allow messages to be sent back and forth, after connecting to a channel",
       async () => {
         const promise = deferred();
-
         const WSClient = new WebSocket(
           `ws://${WSServer.hostname}:${WSServer.port}`,
         );
-
         WSClient.onopen = function () {
           WSClient.send(JSON.stringify({
             connect_to: ["chan1"],
@@ -84,7 +77,6 @@ Rhum.testPlan("app_3000", () => {
             },
           }));
         };
-
         let messageCount = 0;
         WSClient.onmessage = function (message: any) {
           messageCount++;
@@ -97,15 +89,80 @@ Rhum.testPlan("app_3000", () => {
             WSClient.close();
           }
         };
-
         WSClient.onclose = function () {
           promise.resolve();
         };
-
         await promise;
       },
     );
-  });
+    // Rhum.testCase("Cannot send messages to channels a user hasn't connected to", async () => {
+    //   const promise = deferred();
+    //   const WSClient1 = new WebSocket(
+    //       `ws://${WSServer.hostname}:${WSServer.port}`,
+    //   );
+    //   const WSClient2 = new WebSocket(
+    //       `ws://${WSServer.hostname}:${WSServer.port}`,
+    //   );
+    //   // Have one client connected so we can ensure they dont get a message
+    //   WSClient1.onopen = function () {
+    //     WSClient1.send(JSON.stringify({
+    //       connect_to: ["chan1"]
+    //     }));
+    //   }
+    //   WSClient2.onopen = function () {
+    //     WSClient2.send(JSON.stringify({
+    //       send_packet: {
+    //         to: "chan1",
+    //         message: "This message was sent from the client, but the client shouldn't be allowed to do this"
+    //       },
+    //     }));
+    //   };
+    //   WSClient2.onmessage = function (message) {
+    //     Rhum.asserts.assertEquals(message.data, 'Client 9 is not connected to chan1');
+    //     WSClient2.close();
+    //   };
+    //   WSClient1.onmessage = function (message) {
+    //     console.log('ws client 1 got message')
+    //     console.log(message)
+    //     WSClient1.close()
+    //   }
+    //   WSClient1.onclose = function () {
+    //     promise.resolve();
+    //   };
+    //   await promise;
+    // })
+    Rhum.testCase("Does not allow connecting and sending a message to a channel that isn't opened", async () => {
+      const promise = deferred();
+      const WSClient = new WebSocket(
+          `ws://${WSServer.hostname}:${WSServer.port}`,
+      );
+      WSClient.onopen = function () {
+        WSClient.send(JSON.stringify({
+          connect_to: ["chan2"],
+        }));
+        WSClient.send(JSON.stringify({
+          send_packet: {
+            to: "chan2",
+            message: "Message to chan2 from client"
+          }
+        }))
+      };
+      let messageCount = 0;
+      WSClient.onmessage = function (message: any) {
+        messageCount++;
+        if (messageCount === 1) {
+          Rhum.asserts.assertEquals(message.data, 'Channel \"chan2\" does not exist.')
+        } else {
+          Rhum.asserts.assertEquals(message.data, "Client 8 is not connected to chan2");
+          WSClient.close();
+        }
+      };
+      WSClient.onclose = function () {
+        promise.resolve();
+      };
+      await promise;
+    })
+  })
 });
 
 Rhum.run();
@@ -113,7 +170,6 @@ Rhum.run();
 Deno.test({
   name: "Stop the server",
   async fn() {
-    console.log("hmm");
     try {
       WSServer.close();
     } catch (error) {
