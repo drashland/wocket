6f33db85a315b4e2d5d7aea1bac0841ea0fbc180
Edward Bebbington <edward.bebbington@intercity.technology>
2020-10-03 07:33:34 +0100

    dneo fmt

--- deps.ts
+++ deps.ts
@@ -1,13 +1,13 @@
 export {
   HTTPOptions,
   HTTPSOptions,
-  Server as DenoServer,
   serve,
+  Server as DenoServer,
   serveTLS,
 } from "https://deno.land/std@0.72.0/http/server.ts";
 
 export {
-  WebSocket,
   acceptWebSocket,
   isWebSocketCloseEvent,
+  WebSocket,
 } from "https://deno.land/std@0.72.0/ws/mod.ts";
--- tests/deps.ts
+++ tests/deps.ts
@@ -1,14 +1,10 @@
 export {
-  WebSocket,
   acceptWebSocket,
   isWebSocketCloseEvent,
   serve,
+  WebSocket,
 } from "../deps.ts";
 
-export {
-  Rhum,
-} from "https://deno.land/x/rhum@v1.1.4/mod.ts";
+export { Rhum } from "https://deno.land/x/rhum@v1.1.4/mod.ts";
 
-export {
-  deferred,
-} from "https://deno.land/std@0.72.0/async/deferred.ts";
+export { deferred } from "https://deno.land/std@0.72.0/async/deferred.ts";
