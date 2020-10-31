7ba0926dbaec41e29a16b152133442dbcaa4603f
Eric Crooks <eric.crooks@gmail.com>
2020-10-31 01:04:56 -0400

    fixing tests

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,9 +1,6 @@
-export {
-  acceptWebSocket,
-  isWebSocketCloseEvent,
-  serve,
-  WebSocket,
-} from "../deps.ts";
+export { acceptWebSocket, isWebSocketCloseEvent, serve } from "../deps.ts";
+
+export type { WebSocket } from "../deps.ts";
 
 export { Rhum } from "https://deno.land/x/rhum@v1.1.4/mod.ts";
 
