a91e56b4b020979ad72f8ac133391fd435edb824
Edward Bebbington <edward.bebbington@intercity.technology>
2020-11-26 17:08:38 +0000

    Remove unused drash dep in tests

--- tests/deps.ts
+++ tests/deps.ts
@@ -5,5 +5,3 @@ export type { WebSocket } from "../deps.ts";
 export { Rhum } from "https://deno.land/x/rhum@v1.1.4/mod.ts";
 
 export { deferred } from "https://deno.land/std@0.79.0/async/deferred.ts";
-
-export { Drash } from "https://deno.land/x/drash@v1.2.5/mod.ts";
