b49653d2932fd09485df96b113d52a92a2d3c8d6
Edward Bebbington <edward.bebbington@intercity.technology>
2021-01-14 17:25:24 +0000

    [#111-add-socket-client] Add basic socket client; start writing tests

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,12 +6,100 @@ interface Configs {
   reconnect: boolean,
   protocol: string,
 }
+//
+// interface Options {
+//   hostname?: string,
+//   port?: number,
+//   reconnect?: boolean,
+//   protocol?: string
+// }
 
 interface Options {
   hostname?: string,
   port?: number,
-  reconnect?: boolean,
-  protocol?: string
+  protocol?: "ws" | "wss",
+  path?: string
+}
+
+export class WocketClient {
+
+  public websocket:  WebSocket | null= null
+
+  public listeners: Record<string, (data: Record<string, unknown>) => void> = {}
+
+  constructor() {
+
+  }
+
+  public async connect(opts: Options, channels: string[]): Promise<void> {
+    if (!opts.hostname) opts.hostname = "localhost"
+    if (!opts.port) opts.port = 3000
+    if (!opts.protocol) opts.protocol = "ws"
+    if (!opts.path) opts.path = ""
+
+    // Connect
+    const connected = deferred()
+    const url = `${opts.protocol}://${opts.hostname}:${opts.port}${opts.path}`
+    this.websocket = new WebSocket(url)
+    this.websocket.onopen = function () {
+      connected.resolve()
+    }
+    this.websocket.onerror = function () {
+      throw new Error("Error.. todo")
+    }
+    await connected
+
+    // Connect to channels
+    const channelsSetup = deferred()
+    let channelsConnectedTo = 0
+    this.websocket.onmessage = function (event) {
+      if (typeof event.data === "string" && event.data.includes("Connected to")) {
+        channelsConnectedTo++
+        if (channels.length ===  channelsConnectedTo) {
+          channelsSetup.resolve()
+        }
+      }
+    }
+    this.websocket.send(JSON.stringify({
+      connect_to: channels
+    }))
+    await channelsSetup
+
+    // initialise message listener
+    this.websocket.onmessage = (event) => {
+      const json = JSON.parse(event.data)
+      const channel = json.to
+      const from = json.from
+      const message = json.message
+      if (this.listeners[channel]) {
+        this.listeners[channel](message)
+      }
+    }
+  }
+
+  public to (channel: string, data: Record<string, unknown>): void {
+    const message = {
+      send_packet: {
+        to: channel,
+        message: data
+      }
+    }
+    const msgStr = JSON.stringify(message)
+    this.websocket!.send(msgStr)
+  }
+
+  public on (channel: string, cb: (data: Record<string, unknown>) => void): void {
+    this.listeners[channel] = cb
+  }
+
+  public async close ()  {
+    const p = deferred()
+    this.websocket!.onclose = function () {
+      p.resolve()
+    }
+    this.websocket!.close()
+    await p
+  }
 }
 
 /**
@@ -38,225 +126,226 @@ interface Options {
  *    - Learn more about writing web socket client applications at the following address:
  *          https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_client_applications
  */
-export default class WocketClient {
-  /**
-   * Configs for the client
-   */
-  public readonly configs: Configs;
-
-  public readonly decoder: TextDecoder
-
-  /**
-   * Keep track of how many times we reconnect
-   */
-  public reconnectCount: number;
-
-  /**
-   * What channels we are listening to
-   */
-  public listening_to: Record<string, (data: Record<string, unknown>) => void>
-
-  /**
-   * Queue to hold the messages, to eventually fire off
-   */
-  public message_queue: Uint8Array[]
-
-  /**
-   * Is the connection ready?
-   */
-  public ready: boolean
-
-  /**
-   * The connected client
-   */
-  public connection: WebSocket | null = null;
-
-  /**
-   * @description
-   *     Construct an object of this class.
-   */
-  constructor(options: Options) {
-    this.configs = {
-      hostname: options.hostname || "localhost",
-      port: options.port || 3000,
-      reconnect: options.reconnect || true,
-      protocol: options.protocol || "ws",
-    };
-    this.decoder = new TextDecoder();
-    this.reconnectCount = 0;
-    this.listening_to = {};
-    this.message_queue = [];
-    this.ready = true;
-  }
-
-  public async connect() {
-    await this._connectToSocketServer()
-  }
-
-  // FILE MARKER - METHODS FOR PUBLIC USE //////////////////////////////////////////////////////////
-
-  /**
-   * @description
-   *     On receipt of a message in the following channel or on the following event, perform the
-   *     specified callback.
-   *
-   * @param {String} channelOrEvent
-   *     The name of the channel or event.
-   * @param {Function} callback
-   *     The callback to execute on receipt of a message from the channel or event.
-   */
-  public on(channelOrEvent: string, callback: (data: Record<string, unknown>) => void) {
-    if (this._isClientReady()) {
-      // if (!this.listening_to![channelOrEvent]) {
-      //   this.listening_to[channelOrEvent] = null;
-      // }
-      this.listening_to[channelOrEvent] = callback;
-      const message = JSON.stringify({ listening_to: channelOrEvent });
-      const encoded = new TextEncoder().encode(message);
-      this.message_queue.push(encoded);
-      this._sendMessagesToSocketServer();
-    } else {
-      setTimeout(() => this.on(channelOrEvent, callback), 5);
-    }
-  }
-
-  /**
-   * @description
-   *     Send a message to a channel or an event.
-   *
-   * @param {String} channelOrEvent
-   *     The name of the channel or event.
-   * @param {String} message
-   *     The message to send to the channel or event.
-   */
-  public to(channelOrEvent: string, message: {[key: string]: unknown} | Event): void {
-    const messageString = JSON.stringify({ [channelOrEvent]: message });
-    const encodedMessage = new TextEncoder().encode(messageString);
-    this.message_queue.push(encodedMessage);
-    this._sendMessagesToSocketServer();
-  }
-
-  public connectTo(channelNames: string[]) {
-    const messageString = JSON.stringify({
-      connect_to: channelNames
-    })
-    const encodedMessage = new TextEncoder().encode(messageString)
-    this.message_queue.push(encodedMessage)
-    this._sendMessagesToSocketServer()
-  }
-
-  public async close() {
-    const p = deferred()
-    this.connection!.onclose = function () {
-      p.resolve()
-    }
-    this.connection!.close()
-    await p
-  }
-
-  // FILE MARKER - METHODS FOR INTERNAL USE ////////////////////////////////////////////////////////
-
-  /**
-   * Check if connection is ready for events.
-   */
-  private _isClientReady(): boolean {
-    return this.connection!.readyState === 1;
-  }
-
-  private _reconnectSuccessful(previousId: number): void {
-    this.reconnectCount += 1;
-    // server can react if needed to this connection id
-    this.to("reconnect", {
-      previousId,
-      //id: this.connection.id,
-      reconnectCount: this.reconnectCount,
-    });
-  }
-
-  /**
-   * Connect to the socket server at the hostname and port specified in the configs.
-   */
-  private async _connectToSocketServer(reconnect?: boolean) {
-    //const previousId = reconnect ? this.connection.id : null;
-    this.connection = new WebSocket(
-        `${this.configs.protocol}://${this.configs.hostname}:${this.configs.port}`,
-    );
-    const p = deferred()
-    this.connection.onopen = function () {
-      p.resolve()
-    }
-    await p
-
-    //if (previousId) {
-    //  this._reconnectSuccessful(previousId);
-    //}
-    //this._listenToSocketClientEvents();
-  }
-
-  /**
-   * @description
-   *     Listen to events attached to the client.
-   */
-  _listenToSocketClientEvents() {
-    this.connection!.addEventListener("message", (event) => {
-      this._handleEncodedMessage(event.data);
-    });
-    this.connection!.addEventListener("error", (event) => {
-      // send error message to server
-      this.to("error", event);
-    });
-    this.connection!.addEventListener("close", () => {
-      this._connectToSocketServer(true);
-    });
-  }
-
-  /**
-   * @description
-   *     All messages received by the socket server will be handled by this method.
-   *
-   * @param message
-   *     The encoded message. See https://developer.mozilla.org/en-US/docs/Web/API/Body for more
-   *     information about the Body mixin.
-   */
-  private _handleEncodedMessage(message: string | Body) {
-    if (typeof message === "string" && "ping") {
-      this._pongServer();
-    } else if (typeof message === "object") {
-      message.arrayBuffer().then((buffer: ArrayBuffer) => {
-        const decodedMessage = this.decoder.decode(buffer);
-        const parsedMessage = JSON.parse(decodedMessage);
-        Object.keys(parsedMessage).forEach((channelOrEvent) => {
-          if (this.listening_to[channelOrEvent]) {
-            this.listening_to[channelOrEvent](parsedMessage[channelOrEvent]);
-          }
-        });
-      });
-    }
-  }
-
-  /**
-   * @description
-   *     Send all messages in the message queue to the socket server.
-   */
-  private _sendMessagesToSocketServer(): void {
-    if (this._isClientReady() && this.ready && this.message_queue.length) {
-      this.ready = false;
-      let message = null;
-      while (this.message_queue.length) {
-        message = new Uint8Array(this.message_queue[0].length);
-        message.set(this.message_queue.pop() as Uint8Array);
-      }
-      this.connection!.send(message as Uint8Array);
-      this.ready = true;
-      this._sendMessagesToSocketServer();
-    }
-  }
-
-  /**
-   * @description
-   *     Send pong message to server.
-   */
-  private _pongServer(): void {
-    this.connection!.send("pong");
-  }
-}
+// export default class WocketClient {
+//   /**
+//    * Configs for the client
+//    */
+//   public readonly configs: Configs;
+//
+//   public readonly decoder: TextDecoder
+//
+//   /**
+//    * Keep track of how many times we reconnect
+//    */
+//   public reconnectCount: number;
+//
+//   /**
+//    * What channels we are listening to
+//    */
+//   public listening_to: Record<string, (data: Record<string, unknown>) => void>
+//
+//   /**
+//    * Queue to hold the messages, to eventually fire off
+//    */
+//   public message_queue: Uint8Array[]
+//
+//   /**
+//    * Is the connection ready?
+//    */
+//   public ready: boolean
+//
+//   /**
+//    * The connected client
+//    */
+//   public connection: WebSocket | null = null;
+//
+//   private wait_to_connect_to_channels: any = {}
+//
+//   /**
+//    * @description
+//    *     Construct an object of this class.
+//    */
+//   constructor(options: Options) {
+//     this.configs = {
+//       hostname: options.hostname || "localhost",
+//       port: options.port || 3000,
+//       reconnect: options.reconnect ?? true,
+//       protocol: options.protocol || "ws",
+//     };
+//     this.decoder = new TextDecoder();
+//     this.reconnectCount = 0;
+//     this.listening_to = {};
+//     this.message_queue = [];
+//     this.ready = true;
+//   }
+//
+//   public async connect() {
+//     await this._connectToSocketServer()
+//     this._listenToSocketClientEvents()
+//   }
+//
+//   // FILE MARKER - METHODS FOR PUBLIC USE //////////////////////////////////////////////////////////
+//
+//   /**
+//    * @description
+//    *     On receipt of a message in the following channel or on the following event, perform the
+//    *     specified callback.
+//    *
+//    * @param {String} channelOrEvent
+//    *     The name of the channel or event.
+//    * @param {Function} callback
+//    *     The callback to execute on receipt of a message from the channel or event.
+//    */
+//   public on(channelOrEvent: string, callback: (data: Record<string, unknown>) => void) {
+//     if (this._isClientReady()) {
+//       // if (!this.listening_to![channelOrEvent]) {
+//       //   this.listening_to[channelOrEvent] = null;
+//       // }
+//       this.listening_to[channelOrEvent] = callback;
+//       const message = JSON.stringify({ connect_to: [channelOrEvent] });
+//       const encoded = new TextEncoder().encode(message);
+//       this.message_queue.push(encoded);
+//       this._sendMessagesToSocketServer();
+//     } else {
+//       setTimeout(() => this.on(channelOrEvent, callback), 5);
+//     }
+//   }
+//
+//   /**
+//    * @description
+//    *     Send a message to a channel or an event.
+//    *
+//    * @param {String} channelOrEvent
+//    *     The name of the channel or event.
+//    * @param {String} message
+//    *     The message to send to the channel or event.
+//    */
+//   public to(channelOrEvent: string, message: {[key: string]: unknown} | Event): void {
+//     const messageString = JSON.stringify({
+//       send_packet: {
+//         to: channelOrEvent,
+//         message: message
+//       }
+//     });
+//     const encodedMessage = new TextEncoder().encode(messageString);
+//     this.message_queue.push(encodedMessage);
+//     this._sendMessagesToSocketServer();
+//   }
+//
+//   public async close() {
+//     this.configs.reconnect = false
+//     const p = deferred()
+//     this.connection!.onclose = function () {
+//       p.resolve()
+//     }
+//     this.connection!.close()
+//     await p
+//   }
+//
+//   // FILE MARKER - METHODS FOR INTERNAL USE ////////////////////////////////////////////////////////
+//
+//   /**
+//    * Check if connection is ready for events.
+//    */
+//   private _isClientReady(): boolean {
+//     return this.connection!.readyState === 1;
+//   }
+//
+//   private _reconnectSuccessful(previousId: number): void {
+//     this.reconnectCount += 1;
+//     // server can react if needed to this connection id
+//     this.to("reconnect", {
+//       previousId,
+//       //id: this.connection.id,
+//       reconnectCount: this.reconnectCount,
+//     });
+//   }
+//
+//   /**
+//    * Connect to the socket server at the hostname and port specified in the configs.
+//    */
+//   private async _connectToSocketServer(reconnect?: boolean) {
+//     //const previousId = reconnect ? this.connection.id : null;
+//     this.connection = new WebSocket(
+//         `${this.configs.protocol}://${this.configs.hostname}:${this.configs.port}`,
+//     );
+//     const p = deferred()
+//     this.connection.onopen = function () {
+//       p.resolve()
+//     }
+//     await p
+//
+//     //if (previousId) {
+//     //  this._reconnectSuccessful(previousId);
+//     //}
+//     //this._listenToSocketClientEvents();
+//   }
+//
+//   /**
+//    * @description
+//    *     Listen to events attached to the client.
+//    */
+//   private _listenToSocketClientEvents() {
+//     this.connection!.addEventListener("message", (event) => {
+//       if (typeof event.data === "string" && event.data.indexOf("Connected to") >= 0) {
+//
+//       }
+//       console.log('got msgL ' + event.data)
+//       this._handleEncodedMessage(event.data);
+//     });
+//     this.connection!.addEventListener("error", (event) => {
+//       // send error message to server
+//       this.to("error", event);
+//     });
+//     this.connection!.addEventListener("close", () => {
+//       if (this.configs.reconnect === true) {
+//         this._connectToSocketServer(true);
+//       }
+//     });
+//   }
+//
+//   /**
+//    * @description
+//    *     All messages received by the socket server will be handled by this method.
+//    *
+//    * @param message
+//    *     The encoded message. See https://developer.mozilla.org/en-US/docs/Web/API/Body for more
+//    *     information about the Body mixin.
+//    */
+//   private _handleEncodedMessage(message: string | Body) {
+//     if (typeof message === "string" && message === "ping") {
+//       this._pongServer();
+//     } else if (typeof message === "object") {
+//       message.arrayBuffer().then((buffer: ArrayBuffer) => {
+//         const decodedMessage = this.decoder.decode(buffer);
+//         const parsedMessage = JSON.parse(decodedMessage);
+//         Object.keys(parsedMessage).forEach((channelOrEvent) => {
+//           if (this.listening_to[channelOrEvent]) {
+//             this.listening_to[channelOrEvent](parsedMessage[channelOrEvent]);
+//           }
+//         });
+//       });
+//     }
+//   }
+//
+//   /**
+//    * @description
+//    *     Send all messages in the message queue to the socket server.
+//    */
+//   private _sendMessagesToSocketServer(): void {
+//     if (this._isClientReady() && this.ready && this.message_queue.length) {
+//       this.ready = false;
+//       this.connection!.send(new TextDecoder().decode(this.message_queue.pop()));
+//       this.ready = true;
+//       this._sendMessagesToSocketServer();
+//     }
+//   }
+//
+//   /**
+//    * @description
+//    *     Send pong message to server.
+//    */
+//   private _pongServer(): void {
+//     this.connection!.send("pong");
+//   }
+// }
--- tests/unit/client/client_test.ts
+++ tests/unit/client/client_test.ts
@@ -1,34 +1,206 @@
 import {deferred, Rhum} from "../../deps.ts";
-import WocketClient from "../../../src/client/client.ts";
 import { Server } from "../../../src/server/server.ts";
+import {WocketClient} from "../../../src/client/client.ts";
 
 const configs = {
   hostname: "localhost",
   port: 1447
 }
 
+// const server = new Server()
+// await server.run(configs)
+// const p = deferred()
+// server.on("chat", (packet) => {
+//   p.resolve()
+// })
+// const client = new WocketClient(configs)
+// await client.connect()
+// client.connectTo(["chat"])
+// client.to("chat", {
+//   data: "hee"
+// })
+// await p
+// await client.close()
+// await server.close()
+
 Rhum.testPlan("tests/unit/client/client_test.ts", () => {
-  Rhum.testSuite("constructor()", () => {
-    Rhum.testCase("Sets the configs and properties correctly", async () => {
+  // Rhum.testSuite("constructor()", () => {
+  //   Rhum.testCase("Sets the configs correctly w/options", async () => {
+  //     const client = new WocketClient({
+  //       hostname: "beeop boop",
+  //       port: 1234,
+  //       reconnect: false,
+  //       protocol: "wss"
+  //     })
+  //     Rhum.asserts.assertEquals(client.configs, {
+  //       hostname: "beeop boop",
+  //       port: 1234,
+  //       reconnect: false,
+  //       protocol: "wss"
+  //     })
+  //   });
+  //   Rhum.testCase("Sets the configs  correctly w/o options", () => {
+  //     const client = new WocketClient({})
+  //     Rhum.asserts.assertEquals(client.configs, {
+  //       hostname: "localhost",
+  //       port: 3000,
+  //       reconnect: true,
+  //       protocol: "ws"
+  //     })
+  //   })
+  // });
+  // Rhum.testSuite("connect()", () => {
+  //   Rhum.testCase("Should connect to the server", async () => {
+  //     const server = new Server()
+  //     await server.run(configs)
+  //     const client = new WocketClient(configs)
+  //     await client.connect()
+  //     Rhum.asserts.assertEquals(client.connection!.readyState, 1)
+  //     await client.close()
+  //     await server.close()
+  //   })
+  // })
+  // Rhum.testSuite("to()", async () => {
+  //   Rhum.testCase("Should send a message to the channel on the server", async () => {
+  //     const server = new Server()
+  //     await server.run(configs)
+  //     const client = new WocketClient(configs)
+  //     await client.connect()
+  //     const p = deferred()
+  //     server.on("chat", (packet) => {
+  //       p.resolve(packet.message)
+  //     })
+  //     client.connectTo(["chat"])
+  //     client.to("chat", {
+  //       name: "Washington"
+  //     })
+  //     const message = await p
+  //     console.log('closing cllientt in test')
+  //     await client.close()
+  //     await server.close()
+  //     Rhum.asserts.assertEquals(message, {
+  //       name: "Washington"
+  //     })
+  //   })
+  // })
+  // Rhum.testSuite("connectTo()", () => {
+  //   Rhum.testCase("Should connect to the specified channels to allow sending messages", async () => {
+  //     const server = new Server()
+  //     await server.run(configs)
+  //     const client = new WocketClient(configs)
+  //     await client.connect()
+  //     const p1 = deferred()
+  //     server.on("chat", (packet) => {
+  //       p1.resolve(packet.message)
+  //     })
+  //     const p2 = deferred()
+  //     server.on("users", (packet) => {
+  //       p2.resolve(packet.message)
+  //     })
+  //     client.connectTo(["chat", "users"])
+  //     client.to("chat", {
+  //       name: "Washington"
+  //     })
+  //     client.to("users", {
+  //       notify: "Good day sir"
+  //     })
+  //     const message1 = await p1
+  //     const message2 = await p2
+  //     await client.close()
+  //     await server.close()
+  //     Rhum.asserts.assertEquals(message1, {
+  //       name: "Washington"
+  //     })
+  //     Rhum.asserts.assertEquals(message2, {
+  //       notify: "Good day sir"
+  //     })
+  //   })
+  // })
+  // Rhum.testSuite("close()", () => {
+  //   Rhum.testCase("Should close the server", async () => {
+  //     const server = new Server()
+  //     await server.run(configs)
+  //     const client = new WocketClient(configs)
+  //     await client.connect()
+  //     Rhum.asserts.assertEquals(client.connection!.readyState, 1)
+  //     await client.close()
+  //     Rhum.asserts.assertEquals(client.connection!.readyState, 3)
+  //     await server.close()
+  //   })
+  // })
+  Rhum.testSuite("connect()", () => {
+    Rhum.testCase("Should connect to the server", async () => {
       const server = new Server()
       await server.run(configs)
+      const c = new WocketClient()
+      await c.connect({
+        hostname: "localhost",
+        port: 1447,
+        protocol: "ws"
+      }, [])
+      Rhum.asserts.assertEquals(c.websocket!.readyState, 1)
+      await c.close()
+      await server.close()
+    })
+    Rhum.testCase("It should set the hostname to 'localhost' by default", async () => {
+      const server = new Server()
+      await server.run(configs)
+      const c = new WocketClient()
+      await c.connect({
+        port: 1447,
+        protocol: "ws"
+      }, [])
+      Rhum.asserts.assertEquals(c.websocket!.readyState, 1)
+      await c.close()
+      await server.close()
+    })
+    Rhum.testCase("It should set the protocol to  'ws' by default", async () => {
+      const server = new Server()
+      await server.run(configs)
+      const c = new WocketClient()
+      await c.connect({
+        hostname: "localhost",
+        port: 1447,
+      }, [])
+      Rhum.asserts.assertEquals(c.websocket!.readyState, 1)
+      await c.close()
+      await server.close()
+    })
+  })
+  Rhum.testSuite("on()", () => {
+    Rhum.testCase("Should register a listener and handle events for that channel", async () => {
+      const server = new Server()
+      await server.run(configs)
+      server.on("chat", (packet) => {})
+      server.on("users", (packet) => {})
+      const c = new WocketClient()
+      await c.connect({
+        hostname: "localhost",
+        port: 1447,
+        protocol: "ws"
+      }, ["chat"])
       const p = deferred()
-      server.on("chat", (packet) => {
-        console.log('dataaaa')
-        p.resolve()
+      c.on("chat", (data) => {
+        p.resolve(data)
       })
-      const client = new WocketClient(configs)
-      await client.connect()
-      client.connectTo(["chat"])
-      client.to("chat", {
-        data: "hee"
+      Rhum.asserts.assertEquals(!!c.listeners["chat"], true)
+      server.to("chat", {
+        d: "dd"
       })
       await p
-      //Rhum.asserts.assertEquals(Cl.name, "my channel");
-      //Rhum.asserts.assertEquals(channel.listeners, {});
-      //Rhum.asserts.assertEquals(channel.callbacks, []);
-    });
-  });
+      const message = await p
+      Rhum.asserts.assertEquals(message, {
+        d: "dd"
+      })
+      await c.close()
+      await server.close()
+    })
+  })
+  Rhum.testSuite("to()", () => {
+    Rhum.testCase("It should send a message to the socket server when connected to the channel", async () => {
+
+    })
+  })
 });
 
 Rhum.run();
