ebb0e27e9b5510e2e3b023c1a5d4701907099b49
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-06-27 23:59:31 +0100

    Add unit tests for channel class

--- tests/test_helpers.ts
+++ tests/test_helpers.ts
@@ -1,4 +1,4 @@
-import { WebSocket } from "../deps.ts";
+import { deferred } from "./deps.ts"
 
 export const fakeClientSocket = () => {
   return {
@@ -10,5 +10,55 @@ export const fakeClientSocket = () => {
     },
   } as unknown as WebSocket;
 };
+
+/**
+ * Will wait until the client has opened a connection to the server,
+ * so you easily create a new client, call this fn (in 1 line of code)
+ * and then on, do your usual stuff
+ * 
+ * @param client - The client websocket
+ */
+export const waitForClientToOpen = async (client: WebSocket): Promise<void> => {
+  const p = deferred()
+  client.onopen = () => p.resolve()
+  await p
+}
+
+export const connectToChannels = async (client: WebSocket, channels: string[]) => {
+  const defs = new Array(channels.length).fill(deferred())
+  client.send(JSON.stringify({
+    connect_to: channels
+  }))
+  let msgCount = 0
+  client.onmessage = () => {
+    msgCount++
+    defs[msgCount - 1].resolve()
+  }
+  for (const def of defs) {
+    await def
+  }
+}
+
+export const closeClient = async (client: WebSocket) => {
+  const p = deferred()
+  client.onclose = () => p.resolve()
+  await p
+}
+
+export const closeClients = async (...clients: WebSocket[]) => {
+  for (const client of clients) {
+    await closeClient(client)
+  }
+}
+
+export const waitForMsg = async (client: WebSocket): Promise<unknown> => {
+  const p = deferred()
+  client.onmessage = (event) => {
+    const message = { event }
+    p.resolve(message)
+  }
+  const message = await p
+  return message
+}
 
 
