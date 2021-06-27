236ec8d316a238724a37e3a31a500a6fcfbbfe7e
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-06-28 00:30:04 +0100

    Partway through adding unit tests for server

--- tests/unit/server_test.ts
+++ tests/unit/server_test.ts
@@ -1,5 +1,6 @@
 import { Server } from "../../mod.ts";
 import { Rhum } from "../deps.ts";
+import { serve } from "../../deps.ts"
 
 Rhum.testPlan("unit/server_test.ts", () => {
   Rhum.testSuite("constructor()", () => {
@@ -11,12 +12,31 @@ Rhum.testPlan("unit/server_test.ts", () => {
     Rhum.testCase(
       "should connect to localhost if hostname is not provided",
       () => {
-        const expect = "localhost";
+        const expect = "0.0.0.0";
         const server = new Server();
         Rhum.asserts.assertEquals(server.hostname, expect);
       },
     );
   });
+  Rhum.testSuite("close()", () => {
+    Rhum.testCase("Should close the deno server", async () => {
+      const server = new Server()
+      server.deno_server = serve({ port: 8000 })
+      server.close()
+      // If server is closed, this for loop should be skipped. If server was not closed, this loop will be executed and will hang indefinitely
+      for await (const _req of server.deno_server) {}
+    })
+    Rhum.testCase("Should not error if server is not set or not defined", () => {
+      const server = new Server()
+      server.close()
+    })
+    Rhum.testCase("Should not error if server is already closed", () => {
+      const server = new Server()
+      server.deno_server = serve({ port: 8000 })
+      server.close()
+      server.close()
+    })
+  })
 });
 
 Rhum.run();
