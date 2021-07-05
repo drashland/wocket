6cae2908ecd009d898821fceac4670968e3f9acf
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-07-05 16:07:37 +0100

    deno fmt

--- tests/integration/app_3000/tests.ts
+++ tests/integration/app_3000/tests.ts
@@ -22,60 +22,66 @@ async function createWebSocketServer(
   return WSServer;
 }
 
-async function waitForMessage(client: WebSocket): Promise<Record<string, unknown>> {
-  const p = deferred<any>()
+async function waitForMessage(
+  client: WebSocket,
+): Promise<Record<string, unknown>> {
+  const p = deferred<any>();
   client.onmessage = (event) => {
-    const data = JSON.parse(event.data)
-    p.resolve(data.message)
-  }
-  const result = await p
-  return result
+    const data = JSON.parse(event.data);
+    p.resolve(data.message);
+  };
+  const result = await p;
+  return result;
 }
 
-function sendMsg (client: WebSocket, channel: string, message: any) {
+function sendMsg(client: WebSocket, channel: string, message: any) {
   client.send(JSON.stringify({
     send_packet: {
       to: channel,
-      message: message
-    }
-  }))
+      message: message,
+    },
+  }));
 }
 
-async function sendMsgAndGetRes(client: WebSocket, message: any, channel: string) {
-  sendMsg(client, channel, message)
-  return await waitForMessage(client)
+async function sendMsgAndGetRes(
+  client: WebSocket,
+  message: any,
+  channel: string,
+) {
+  sendMsg(client, channel, message);
+  return await waitForMessage(client);
 }
 
 async function closeClient(client: WebSocket) {
-  const p = deferred()
+  const p = deferred();
   client.onclose = () => {
-    p.resolve()
-  }
-  client.close()
-  await p
+    p.resolve();
+  };
+  client.close();
+  await p;
 }
 
 async function createClient(channelsToConnectTo: string[]) {
   // connect
-  const client = new WebSocket("ws://127.0.0.1:3000")
-  const p1 = deferred()
-  client.onopen = () => p1.resolve()
-  await p1
+  const client = new WebSocket("ws://127.0.0.1:3000");
+  const p1 = deferred();
+  client.onopen = () => p1.resolve();
+  await p1;
   // connect to channels
   client.send(JSON.stringify({
-    connect_to: channelsToConnectTo
-  }))
-  const p2 = deferred()
-  const expectedConnEvents = channelsToConnectTo.length
-  let connEvents = 0
+    connect_to: channelsToConnectTo,
+  }));
+  const p2 = deferred();
+  const expectedConnEvents = channelsToConnectTo.length;
+  let connEvents = 0;
   client.onmessage = (event) => {
-    connEvents++
+    connEvents++;
     if (connEvents === expectedConnEvents) {
-      p2.resolve()
+      p2.resolve();
     }
-  }
-  await p2
-  return client
+  };
+  await p2;
+  return client;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -166,29 +172,39 @@ Rhum.testPlan("app_3000", () => {
           );
         });
         server.on("get-id", (packet: Packet) => {
-          server.to("get-id", { id: packet.from.id }, Number(packet.from.id))
-        })
-        const clientOne = await createClient(["emit to specific client", "get-id"])
-        const clientTwo = await createClient(["emit to specific client", "get-id"])
-        const clientThree = await createClient(["emit to specific client", "get-id"])
+          server.to("get-id", { id: packet.from.id }, Number(packet.from.id));
+        });
+        const clientOne = await createClient([
+          "emit to specific client",
+          "get-id",
+        ]);
+        const clientTwo = await createClient([
+          "emit to specific client",
+          "get-id",
+        ]);
+        const clientThree = await createClient([
+          "emit to specific client",
+          "get-id",
+        ]);
         // Get connected messages out of the way
-        const client1Id = (await sendMsgAndGetRes(clientOne, {}, "get-id")).id
-        const client2Id = (await sendMsgAndGetRes(clientTwo, {}, "get-id")).id
-        const client3Id = (await sendMsgAndGetRes(clientThree, {}, "get-id")).id
+        const client1Id = (await sendMsgAndGetRes(clientOne, {}, "get-id")).id;
+        const client2Id = (await sendMsgAndGetRes(clientTwo, {}, "get-id")).id;
+        const client3Id =
+          (await sendMsgAndGetRes(clientThree, {}, "get-id")).id;
         // Send a message to client 3 and expect only client 3 gets it and not client 1 or 2
-        sendMsg(clientOne, "emit to specific client", client2Id)
-        const client2Response = await waitForMessage(clientTwo)
-        sendMsg(clientTwo, "emit to specific client", client3Id)
-        const client3Response = await waitForMessage(clientThree)
-        sendMsg(clientThree, "emit to specific client", client1Id)
-        const client1Response = await waitForMessage(clientOne)
-        await closeClient(clientOne)
-        await closeClient(clientTwo)
-        await closeClient(clientThree)
+        sendMsg(clientOne, "emit to specific client", client2Id);
+        const client2Response = await waitForMessage(clientTwo);
+        sendMsg(clientTwo, "emit to specific client", client3Id);
+        const client3Response = await waitForMessage(clientThree);
+        sendMsg(clientThree, "emit to specific client", client1Id);
+        const client1Response = await waitForMessage(clientOne);
+        await closeClient(clientOne);
+        await closeClient(clientTwo);
+        await closeClient(clientThree);
         await server.close();
-        Rhum.asserts.assertEquals(client1Response, "Hello from " + client3Id)
-        Rhum.asserts.assertEquals(client2Response, "Hello from " + client1Id)
-        Rhum.asserts.assertEquals(client3Response, "Hello from " + client2Id)
+        Rhum.asserts.assertEquals(client1Response, "Hello from " + client3Id);
+        Rhum.asserts.assertEquals(client2Response, "Hello from " + client1Id);
+        Rhum.asserts.assertEquals(client3Response, "Hello from " + client2Id);
       },
     );
 
@@ -211,7 +227,8 @@ Rhum.testPlan("app_3000", () => {
         };
         client.onmessage = function (message) {
           Rhum.asserts.assertEquals(
-            message.data.includes(" is not connected to chan1"), true
+            message.data.includes(" is not connected to chan1"),
+            true,
           );
           client.close();
         };
@@ -252,7 +269,8 @@ Rhum.testPlan("app_3000", () => {
             );
           } else {
             Rhum.asserts.assertEquals(
-              message.data.includes(" is not connected to chan2"),true
+              message.data.includes(" is not connected to chan2"),
+              true,
             );
             client.close();
           }
