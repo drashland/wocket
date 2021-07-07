2711249a73ff4640c7bbc2e9bee3257b0f0e0fbe
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-07-07 01:04:42 +0100

    minor improvement

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,16 @@ import { Channel } from "./channel.ts";
 import { Client } from "./client.ts";
 import { OnChannelCallback } from "./types.ts";
 
+type IgnoreConstraint = {
+  ignore: boolean
+}
+type OnlyConstraint = {
+  only: boolean
+}
+type Contraints = {
+  id: number
+} & (IgnoreConstraint | OnlyConstraint)
+
 /**
  * This class is responsible for creating a users socket server, maintaining the
  * connections between sockets, and handling packets to and from sockets.
@@ -70,25 +80,20 @@ export class Server {
    * @param channelName 
    * @param cb 
    */
-  public to(channelName: string, message: Record<string, unknown>, constraints?: {
-    id: number,
-    only?: boolean,
-    ignore?: boolean
-  }): void {
+  public to(channelName: string, message: Record<string, unknown>, constraints?: Contraints): void {
     // If sending to a specific client or wish to not send the message to a specific client
     if (constraints) {
       // If wanting to send to only that client, do that
-      if (constraints.only === true) {
+      if ("only" in constraints && constraints.only === true) {
         this.send(constraints.id, channelName, message)
         return
       }
       // otherwise if they want to not send the message to that user, do that
-      if (constraints.ignore === true) {
+      if ("ignore" in constraints && constraints.ignore === true) {
         for (const clientId of this.clients.keys()) {
-          if (clientId === constraints.id) {
-            continue
+          if (clientId !== constraints.id) {
+            this.send(clientId, channelName, message)
           }
-          this.send(clientId, channelName, message)
         }
         return
       }
