24859d83e92dfcc16a9b31a52a36eedf585357c9
drashbot <59404681+drashbot@users.noreply.github.com>
2021-07-13 05:40:37 -0400

    Update dependencies and bump version numbers across repo (#125)
    Co-authored-by: crookse <crookse@users.noreply.github.com>

--- tests/deps.ts
+++ tests/deps.ts
@@ -4,4 +4,4 @@ export type { WebSocket } from "../deps.ts";
 
 export { Rhum } from "https://deno.land/x/rhum@v1.1.10/mod.ts";
 
-export { deferred } from "https://deno.land/std@0.100.0/async/deferred.ts";
+export { deferred } from "https://deno.land/std@0.101.0/async/deferred.ts";
