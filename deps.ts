4c35875df51d707a40431669f30359103476a99d
drashbot <59404681+drashbot@users.noreply.github.com>
2021-08-24 15:30:07 -0400

    Update dependencies and bump version numbers across repo (#130)
    Co-authored-by: crookse <crookse@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -2,16 +2,16 @@ export {
   serve,
   Server as DenoServer,
   serveTLS,
-} from "https://deno.land/std@0.105.0/http/server.ts";
+} from "https://deno.land/std@0.106.0/http/server.ts";
 
 export type {
   HTTPOptions,
   HTTPSOptions,
-} from "https://deno.land/std@0.105.0/http/server.ts";
+} from "https://deno.land/std@0.106.0/http/server.ts";
 
 export {
   acceptWebSocket,
   isWebSocketCloseEvent,
-} from "https://deno.land/std@0.105.0/ws/mod.ts";
+} from "https://deno.land/std@0.106.0/ws/mod.ts";
 
-export type { WebSocket } from "https://deno.land/std@0.105.0/ws/mod.ts";
+export type { WebSocket } from "https://deno.land/std@0.106.0/ws/mod.ts";
