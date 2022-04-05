5425bcf619d18805fbfb66ea7590caa56a3e5dd0
crookse <crookse@users.noreply.github.com>
2022-04-05 01:07:17 +0000

    Update dependencies and bump version numbers across repo

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,6 +1,6 @@
-export { Rhum } from "https://deno.land/x/rhum@v1.1.13/mod.ts";
-export { deferred, delay } from "https://deno.land/std@0.132.0/async/mod.ts";
+export { Rhum } from "https://deno.land/x/rhum@v1.1.14/mod.ts";
+export { deferred, delay } from "https://deno.land/std@0.133.0/async/mod.ts";
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.132.0/testing/asserts.ts";
+} from "https://deno.land/std@0.133.0/testing/asserts.ts";
